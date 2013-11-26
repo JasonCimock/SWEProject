ce1a6dde6921715d97dcd3c0d43e937d2c83c301
arcanell <jcimock@gmail.com>
2013-11-26 14:23:12 -0600

    Update Email.php

--- Email.php
+++ Email.php
@@ -1,6 +1,41 @@
 //sends notification to user or IT dept uses GMAIL for delivery
 
 
-public void sendEmail{
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?require_once "Mail.php";
 
-}
+ $from = "Red Shirt <redshirtspms@gmail.com>";
+ $to = "Jason Cimock <jcimock@gmail.com>";
+ $subject = "Hi!";
+ $body = "Hi,\n\nHow are you?";
+ 
+ $host = "ssl://smtp.gmail.com";
+ $port = "465";
+ $username = "redshirtspms@gmail.com";
+ $password = "softwareengineering";
+ 
+ $headers = array ('From' => $from,
+   'To' => $to,
+   'Subject' => $subject);
+ $smtp = factory('smtp',
+   array ('host' => $host,
+     'port' => $port,
+     'auth' => true,
+     'username' => $username,
+     'password' => $password));
+ 
+ $mail = $smtp->send($to, $headers, $body);
+ 
+ if (PEAR::isError($mail)) {
+   echo("<p>" . $mail->getMessage() . "</p>");
+  } else {
+   echo("<p>Message successfully sent!</p>");
+  }
+?>
+
+</body>
+</html>
